74b0ed3b200dc977da0c282af719a1118f4bb1b6
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-02-05 16:54:01 +0100

    Generic version of smooth_damp

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
+use std::ops::{Add, Mul, Sub};
+
 // from Game programming gems 4
-pub fn smooth_damp(
-    current: f32,
-    target: f32,
-    vel: &mut f32,
-    smooth_time: f32,
-    delta_time: f32,
-) -> f32 {
+// TODO: switch to returning tuples when tuple assignment is stable
+pub fn smooth_damp<T>(current: T, target: T, vel: &mut T, smooth_time: f32, delta_time: f32) -> T
+where
+    T: Sub<T, Output = T> + Add<T, Output = T> + Mul<f32, Output = T> + Add<T, Output = T> + Copy,
+    f32: Mul<T, Output = T> + Mul<f32, Output = f32>,
+{
     let omega = 2. / smooth_time;
     let x = omega * delta_time;
-    let exp = 1. / (1. + x + 0.48 * x * x + 0.235 * x * x * x);
+    let exp = 1. / (1. + x + (0.48 as f32) * x * x + (0.235 as f32) * x * x * x);
     let change = current - target;
-    let temp = (*vel + omega * change) * delta_time;
+    let temp: T = (*vel + (omega * change)) * delta_time;
     *vel = (*vel - omega * temp) * exp;
     target + (change + temp) * exp
 }
@@ -18,6 +19,7 @@ pub fn smooth_damp(
 #[cfg(test)]
 mod tests {
     use approx::assert_relative_eq;
+    // use bevy::math::Vec2;
 
     use crate::smooth_damp;
 
@@ -35,4 +37,20 @@ mod tests {
 
         assert_relative_eq!(current, target);
     }
+
+    // #[test]
+    // fn smooth_damp_vec() {
+    //     let mut current = Vec2::new(0., 1.);
+    //     let target = Vec2::new(1., 0.);
+    //     let delta_time = 0.1;
+    //     let mut vel = Vec2::ZERO;
+    //     let smooth_time = 1.0;
+
+    //     for _ in 0..100 {
+    //         current = smooth_damp(current, target, &mut vel, smooth_time, delta_time);
+    //     }
+
+    //     assert_relative_eq!(current.x, target.x);
+    //     assert_relative_eq!(current.x, target.x);
+    // }
 }
