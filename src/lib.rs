31b8349e08441ea85df76d85a59993aaf50e25f3
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-02-05 15:56:18 +0100

    Add game programming gems version of smoothdamp

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,38 @@
+// from Game programming gems 4
+pub fn smooth_damp(
+    current: f32,
+    target: f32,
+    vel: &mut f32,
+    smooth_time: f32,
+    delta_time: f32,
+) -> f32 {
+    let omega = 2. / smooth_time;
+    let x = omega * delta_time;
+    let exp = 1. / (1. + x + 0.48 * x * x + 0.235 * x * x * x);
+    let change = current - target;
+    let temp = (*vel + omega * change) * delta_time;
+    *vel = (*vel - omega * temp) * exp;
+    target + (change + temp) * exp
+}
+
 #[cfg(test)]
 mod tests {
+    use approx::assert_relative_eq;
+
+    use crate::smooth_damp;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn smooth_damp_converges() {
+        let mut current = 0.0;
+        let target = 1.0;
+        let delta_time = 0.1;
+        let mut vel = 0.;
+        let smooth_time = 1.0;
+
+        for _ in 0..100 {
+            current = smooth_damp(current, target, &mut vel, smooth_time, delta_time);
+        }
+
+        assert_relative_eq!(current, target);
     }
 }
